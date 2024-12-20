fa6e704d7dc2da7ff8a3ba7cbdaaeaa6d2b9a4c7
Logan Sauer <ldsauer2@gmail.com>
2024-12-20 14:23:19 -0700

    just made it look prettier

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -6,46 +6,43 @@ RSpec.describe Ship do
   end
 
   describe "#initialize" do
-    it 'can initialize' do
-      expect(@cruiser).to be_an_instance_of(Ship)
-    end
+        it 'can initialize' do
+            expect(@cruiser).to be_an_instance_of(Ship)
+        end
   end
 
   describe "#has attributes" do
-    it 'has a name' do
-      expect(@cruiser.name).to eq("Cruiser")
-    end
+        it 'has a name' do
+            expect(@cruiser.name).to eq("Cruiser")
+        end
   
 
-    it 'has a specific length' do
-      expect(@cruiser.length).to eq(3)
-    end
+        it 'has a specific length' do
+            expect(@cruiser.length).to eq(3)
+        end
 
-    it 'has a specific amount of health' do
-      expect(@cruiser.health).to eq(3)
-    end
+        it 'has a specific amount of health' do
+            expect(@cruiser.health).to eq(3)
+        end
   end
 
   describe "#ship status" do
-    it 'is sunk?' do
-      expect(@cruiser.sunk?).to eq(false)
+        it 'is sunk?' do
+            expect(@cruiser.sunk?).to eq(false)
 
-      @cruiser.hit
+            @cruiser.hit
 
-      expect(@cruiser.health).to eq(2)
+            expect(@cruiser.health).to eq(2)
 
-      @cruiser.hit
+            @cruiser.hit
 
-      expect(@cruiser.health).to eq(1)
+            expect(@cruiser.health).to eq(1)
+            expect(@cruiser.sunk?).to eq(false)
 
-      expect(@cruiser.sunk?).to eq(false)
+            @cruiser.hit
 
-      @cruiser.hit
-
-      expect(@cruiser.sunk?).to eq(true)
-
-
-    end
+            expect(@cruiser.sunk?).to eq(true)
+        end
   end
 
 end
