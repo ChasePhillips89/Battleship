666d5370a25ed3f067747294e856f6bb1f4d202a
Logan Sauer <ldsauer2@gmail.com>
2024-12-21 11:21:42 -0700

    finished the cell portion of iteration one

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -13,4 +13,24 @@ RSpec.describe Cell do
         end
     end
 
+    describe "#has_attributes" do
+        it "has coordinates" do
+            expect(@cell.coordinate).to eq("B4")
+        end
+
+        it "checks to see if there is a ship" do
+            expect(@cell.ship).to eq(nil)
+            expect(@cell.empty?).to eq(true)
+        end
+
+        it "places a ship" do
+            @cell.place_ship(@cruiser)
+
+            expect(@cell.ship).to eq(@cruiser)
+            expect(@cell.empty?).to eq(false)
+        end
+
+
+    end
+
 end
