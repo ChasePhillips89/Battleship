64e4df1f63bfc40d84288aceb8ad041b3d0efb14
Logan Sauer <ldsauer2@gmail.com>
2024-12-23 12:04:28 -0700

    validating coordinates, all tests passing

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -21,7 +21,11 @@ RSpec.describe Board do
 
         it "checks on cell coordinates" do
             
-            exoect(board.valid_)
+            expect(@board.valid_coordinate?("A1")).to eq(true)
+            expect(@board.valid_coordinate?("D4")).to eq(true)
+            expect(@board.valid_coordinate?("A5")).to eq(false)
+            expect(@board.valid_coordinate?("E1")).to eq(false)
+            expect(@board.valid_coordinate?("A22")).to eq(false)
         end
     end
 
