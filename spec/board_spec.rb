47671ebc41e545534040de5bad78fea1f09fc5d7
Logan Sauer <ldsauer2@gmail.com>
2024-12-23 11:44:27 -0700

    trying to figure out how the cell key/values are tested

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,6 +1,6 @@
 require './lib/ship'
 require './lib/cell'
-require './lib.board'
+require './lib/board'
 
 RSpec.describe Board do 
     before (:each) do 
@@ -8,7 +8,17 @@ RSpec.describe Board do
     end
 
     describe '#initalize' do 
-        expect(@board).to be_an_instance_of(Board)
+        it 'makes a board exist' do 
+            expect(@board).to be_an_instance_of(Board)
+        end
+    end
+
+    describe "#cell_objects" do
+        it "creates the cell objects" do
+
+            # expect(@cells.keys).to eq("A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4")
+            # expect(@board.cells).to_be_a(Hash)
+        end
     end
 
 end
