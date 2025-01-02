fdb73ed55a01e282dc594d96a6df2d703cf343aa
Chase Phillips <chase.thomas.phillips@gmail.com>
2025-01-02 17:59:50 -0500

    set up game class and initialization

---
+++ lib/game.rb
@@ -0,0 +1,20 @@
+class Game 
+  attr_reader
+    def initialize 
+
+    end
+
+      def start_game
+
+      puts ""
+      puts ""
+      puts ""
+      puts "Welcome to BATTLESHIP
+      Enter p to play. Enter q to quit."
+    
+      end
+    
+      
+    
+end
+
